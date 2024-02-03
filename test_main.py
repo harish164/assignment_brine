a6b213aeada123ba01a7a58bc748946fd9a54bc2
Harish choudhary <harishchoudhary164@gmail.com>
2024-02-03 11:40:45 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,23 +5,23 @@ from main import read_csv, compute_monthly_revenue, compute_product_revenue, com
 
 class TestMainFunctions(unittest.TestCase):
     def test_read_csv(self):
-        # Write your test cases for read_csv function
+        # Write our test cases for read_csv function
         pass
 
     def test_compute_monthly_revenue(self):
-        # Write your test cases for compute_monthly_revenue function
+        # Write our test cases for compute_monthly_revenue function
         pass
 
     def test_compute_product_revenue(self):
-        # Write your test cases for compute_product_revenue function
+        # Write our test cases for compute_product_revenue function
         pass
 
     def test_compute_customer_revenue(self):
-        # Write your test cases for compute_customer_revenue function
+        # Write our test cases for compute_customer_revenue function
         pass
 
     def test_top_n_customers_by_revenue(self):
-        # Write your test cases for top_n_customers_by_revenue function
+        # Write our test cases for top_n_customers_by_revenue function
         pass
 
 if __name__ == "__main__":
